a2627c262f60e0768491acc045522529f30b8326
gononde <ngononde@yahoo.com>
2022-09-08 04:37:41 -0700

    First main

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Entry main function
+ * Return : 0 after printing of the function
+ */
+int main (void)
+{puts("\"Programming is like building a multilingual puzzle");
+        return(0);
+
+}
