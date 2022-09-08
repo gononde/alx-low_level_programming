6298993d586616a259935fe21f3c60e021b60dc3
gononde <ngononde@yahoo.com>
2022-09-08 06:42:04 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
- * Entry main point
- * Return : 0 after printing the function
- */
+/*Main function*/
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
-        return(0);
+{	puts("\""Programming is like building a multilingual puzzle");
+	return(0);
 }
