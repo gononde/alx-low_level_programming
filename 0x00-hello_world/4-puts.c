f47ab59b3ee39bb80cc521a1c9984bcb40950d87
gononde <ngononde@yahoo.com>
2022-09-08 05:15:59 -0700

    Second main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,7 @@
  * Entry main point
  * Return : 0 after printing the function
  */
-
-int main(void){
-
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{puts ("\"Programming is like building a multilingual puzzle");
         return(0);
-
 }
