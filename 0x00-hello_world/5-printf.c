bf2240d9eac7addaec70d19a71841cad68b9bdab
gononde <ngononde@yahoo.com>
2022-09-08 07:57:51 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+/**
+ * main - prints caracters in the printf function
+ *
+ * Description: using the printf function
+ * this function print "with proper grammar, but the outcome is a piece of art,"
+ * Return : 0
+ */
+
 int main (void)
-{	printf("\""with proper grammar, but the outcome is a piece of art,\n");
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
