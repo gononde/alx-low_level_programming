322a9af145ae820205d34f65973dd9ec3d0acbad
gononde <ngononde@yahoo.com>
2022-09-09 10:34:56 -0700

    Alphabet_lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - return an alphabet
+ *  Description : Using main function
+ *  this function prints the alphabet in lowercase
+ *  Return: 0 after printf
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low = 'z'; low++)
+		putchar(low);
+		printf("\n");
+
+		return (0);
+
+}
+
