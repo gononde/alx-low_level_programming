22b7d1af0ddd9e239367b6ba593d17867b293549
gononde <ngononde@yahoo.com>
2022-09-11 08:36:54 -0700

    Lowercase_Uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	char ch;
-	for (ch ='a'; ch <= 'z'; ch++) /* Print lowercase */
+	for (ch = 'a'; ch <= 'z'; ch++) /* Print lowercase */
 		{
 			putchar(ch);
 		}
-	for (ch ='A'; ch <='Z'; ch++) /* Uppercase */
+	for (ch = 'A'; ch <= 'Z'; ch++) /* Uppercase */
 	{ 
 		putchar(ch);
 	}
