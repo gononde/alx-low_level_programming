f13d8f26a124d28e3bfaa370a7e84291469e681b
gononde <ngononde@yahoo.com>
2022-09-11 11:06:50 -0700

    Digit numbers from 0 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<tsdio.h>
+/**
+ * main - return digit with putchar
+ *
+ * Description : using main function
+ * this function "prints all single digit numbers of base 10 starting from 0"
+ * Return: 0 after printing
+ */
+int main(void)
+{
+	int e;
+
+	for (e ='0' ; e <= '9' ; e++)
+	{
+		putchar(e);
+	}
+	
+	putchar('\n');
+
+	return (0);
+
+}
+
