2f23e1d1749e679922907f775ab9b64c246b596f
gononde <ngononde@yahoo.com>
2022-09-13 05:59:03 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - prints alphabet
+ *
+ * Description : using main function
+ * this function "prints alphabet"
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; c++)
+	{
+		_putchar('\n', i);
+
+		
+		
+	}
+		return (0);
+	
+}
+
+
+
+
+
+
+}
