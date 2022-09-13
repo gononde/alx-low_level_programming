5cb25bb9f3ee749c424bb54c76db9701ed5372fa
gononde <ngononde@yahoo.com>
2022-09-13 04:23:35 -0700

    Task 0 _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - printing  _putchar
+ *
+ * Description : using main function
+ * this function "prints _putchar"
+ * Return: 0 after printing
+ */
+int main(void)
+{
+	char ch;
+
+	_putchar("_putchar", '\n');
+
+	return (0);
+
+}
