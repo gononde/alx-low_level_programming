c9738a563fbddaa6f4c5b6c076c1880f49726a2b
gononde <ngononde@yahoo.com>
2022-09-13 07:04:27 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * int _islower - Check alpahabet lowercase
+ *
+ * Description -using int _islower function
+ * this function "checks for lowercase character"
+ * Return: returns 1 and 0 depend condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
