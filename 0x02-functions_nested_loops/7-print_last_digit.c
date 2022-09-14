900931d05cfeb96e34b6b2179a1bf257d0b6e272
gononde <ngononde@yahoo.com>
2022-09-14 01:05:26 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,11 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int last_digit = n % 10;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar('0' + x);
-	_putchar('\n');
-	return (x);
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
