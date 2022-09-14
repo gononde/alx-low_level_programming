0fe3c545451950eb40f05cc7c19e083316c845c1
gononde <ngononde@yahoo.com>
2022-09-14 02:34:30 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,11 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(0);
+		_putchar(0);
 		for (mult = 1; mult <= 9; mult++)
 
 		{
-			_putchar(0);
+			/*_putchar(0);*/
 			_putchar(',');
 			_putchar(' ');
 
