37739ba1de9ad93732c05cf33f11aa705af954e0
gononde <ngononde@yahoo.com>
2022-09-15 04:46:34 -0700

    Date of year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,21 +15,21 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		printf("Day of the year: %d", day);
-		printf("Remaining days: %d", 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
 
-		printf("Invalid date: %02/%02/%04\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 	}
 	else
 	{
-	printf("Day of the year: %d", day);
-	printf("Remaining days: %d", 366 - day);
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", 365 - day);
 	}
 }
 
