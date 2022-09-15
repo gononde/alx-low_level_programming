e9a02ccd3acf956d5b74c516d653895a3ace2d32
gononde <ngononde@yahoo.com>
2022-09-15 07:00:17 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,30 @@
+#include <stdio.h.>
+#include <time.h>
+#include <stdlib.h>
 #include "main.h"
+
+void positive_or_negative(int i)
+{
+	int n;
+
+srand(time(0));
+
+n = i;
+
+if (n > 0)
+{	printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("%d is zero\n", n);
+}
+else 
+{	printf("%d is negative\n", n);	
+
+return (0);
+}
+}
+
 /**
  * main - give 0 after test
  *
