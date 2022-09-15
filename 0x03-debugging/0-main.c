87a4975d6e3569d56df2dafa992fba7cdd6459ca
gononde <ngononde@yahoo.com>
2022-09-15 02:33:24 -0700

    Give 0 when condition is true

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - give correct output
+ *
+ * Descritpion : using main function
+ * this function " gives the correct output when given a case of 0"
+ * Return: 0 after printing
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
