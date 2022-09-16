593b27e234d7aa82c1b1886f93d904bacad1c803
gononde <ngononde@yahoo.com>
2022-09-16 07:00:44 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Description : using print_number function
+ * this function "prints the numbers, from 0 to 9"
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar(10);
+}
