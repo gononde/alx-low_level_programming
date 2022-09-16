5ecb680b554002748a3666f6adb36ab6fe4e21d2
gononde <ngononde@yahoo.com>
2022-09-16 06:06:44 -0700

    Test ofr uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _isupper - check code
+ *
+ * Description : using _isupper function
+ * this function "check a character if it is uppercase or not"
+ * @c: character
+ * Return: 1 whether it is, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c >= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
