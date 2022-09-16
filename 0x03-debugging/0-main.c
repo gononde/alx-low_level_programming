c8c8a9078af6e80e0e39833428e61835e305aae4
gononde <ngononde@yahoo.com>
2022-09-16 04:50:08 -0700

    First debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,39 +2,6 @@
 #include <time.h>
 #include <stdlib.h>
 #include "main.h"
-
-/**
- * positive_or_negative - return value after test
- * Description : using positive_or_negative function
- * this function "return 0 when a condition is true"
- * @i: parameter
- * Return: 0 after test
- */
-void positive_or_negative(int i)
-{
-	int n;
-
-srand(time(0));
-
-n = i;
-
-if (n > 0)
-{	printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-	printf("%d is zero\n", n);
-}
-else
-{
-
-
-printf("%d is negative\n", n);
-
-return (0);
-}
-}
-
 /**
  * main - give 0 after test
  *
