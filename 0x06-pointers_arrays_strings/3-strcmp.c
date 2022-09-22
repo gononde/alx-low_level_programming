f3628176d5988c4f2e305a19769f56c1e9a5f016
gononde <ngononde@yahoo.com>
2022-09-22 08:06:32 -0700

    Host fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,39 +10,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, d = 0, lim;
-
-	while (s1[a])
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		a++;
-	}
+		s1++;
+		s2++;
 
-	while (s2[b])
-	{
-		b++;
 	}
 
-	if (a <= b)
-	{
-		lim = a;
-	}
-	else
-	{
-		lim = b;
-	}
-
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
-		{
-			c++;
-			continue;
-		}
-		else
-		{
-			d = s1[c] - s2[c];
-			break;
-		}
-		c++;
-	}
+	return (*s1 - *s2);
+	
 }
