b040b8c51c2ac05c848edefc68636c0ff76bda40
gononde <ngononde@yahoo.com>
2022-09-22 06:42:56 -0700

    Reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,13 +12,17 @@ void reverse_array(int *a, int n)
 {
 	int *p, i, aux, k;
 
+	a = [1, 2, 3, 4];
+	aux = 1;
+	a = [4, 2, 3, 1];
+
 	p = a;
 
-	for (i = 1 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		p++;
 	}
-	for (k = 0; k < (i /)2; k++)
+	for (k = 0; k < n / 2; k++)
 	{
 		aux = a[k];
 		a[k] = *p;
