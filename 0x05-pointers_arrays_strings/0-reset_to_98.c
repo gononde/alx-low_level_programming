52e1d01f26be34cfe7ef4cd647b5d87e0e754932
gononde <ngononde@yahoo.com>
2022-09-22 08:52:33 -0700

    Host fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
 /**
  * reset_to_98 - takes pointer and update to 98
  * Description : using reset_to_98 function
- * this function "takes a pointer to an int as parameter and updates the value it points to to 98"
+ * this function "takes a pointer to an int as parameter"
  * @n: an parameter
  * Return: return 98
  */
 void reset_to_98(int *n)
 {
-	*n = 98 ;
+	*n = 98;
 }
