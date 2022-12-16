2e21de73e0f033eef0f09cc9cb4c90c9dad3188a
gononde <ngononde@yahoo.com>
2022-12-16 02:53:56 -0800

    Prints 0123456789\n

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers
+ * print_numbers - Prints 0123456789\n
  *
- * Description : using print_number function
- * this function "prints the numbers, from 0 to 9"
- * Return: nothing
+ * Return: void
  */
 void print_numbers(void)
 {
