e3efe7a4040e1bc1b5f85a335314379024b75936
gononde <ngononde@yahoo.com>
2022-12-19 04:57:44 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int index;
+	int index = 0;
 
-	for (index = 0; s[index] != '\0'; index++)
-	for (index = index - 1; s[index] != '\0'; index--)
+	while (s[index] != '\0')
+		index++;
+	for (index = index - 1; index >= 0; index--)
 	{
 		_putchar(s[index]);
 	}
