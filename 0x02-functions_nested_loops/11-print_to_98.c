7e96c7ab8a8b6806bc7c869a9ceaef3e5e137713
gononde <ngononde@yahoo.com>
2022-12-20 02:21:35 -0800

    Task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (, n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
@@ -22,13 +22,13 @@ void print_to_98(int n)
 			}
 			else
 			{
-			printf("%d, " n);
+			printf("%d, ", n);
 			}
 		}
 	}
 	else
 	{
-		for (, n >== 98; n--)
+		for (; n >== 98; n--)
 		{
 			if (n == 98)
 			{
@@ -38,7 +38,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-			printf("%d, " n);
+			printf("%d, ", n);
 			}
 	}
 }
